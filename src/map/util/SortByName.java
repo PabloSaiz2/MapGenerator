c03a62039f19bad3644130f17f4be9f41161cc64
PabloSaiz2 <55839661+PabloSaiz2@users.noreply.github.com>
2021-03-15 23:06:47 +0100

    Algunos cambios al sort

--- src/map/util/SortByName.java
+++ src/map/util/SortByName.java
@@ -5,39 +5,24 @@ import java.util.Comparator;
 import map.model.Province;
 
 public class SortByName implements Comparator<Province> {
-	private boolean stringIsLess(String a, String b) {
-		boolean isLess = true;
-		int i = 0;
-		if (a.length() == b.length()) {
-			while (isLess && i < a.length()) {
-				isLess = a.charAt(i) < b.charAt(i);
-				++i;
-			}
-		} else if (a.length() > b.length()) {
-
-			isLess = false;
-		}
-		return isLess;
-	}
-
 	private boolean stringIsGreater(String a, String b) {
-		boolean isGreater = true;
+		boolean isSame = true;
 		int i = 0;
-		if (a.length() == b.length()) {
-			while (isGreater && i < a.length()) {
-				isGreater = a.charAt(i) > b.charAt(i);
-				++i;
+		int length = Math.min(a.length(), b.length());
+		boolean isGreater = false;
+		while (isSame && i < length) {
+			isSame = a.charAt(i) == b.charAt(i);
+			if(!isSame) {
+				isGreater = a.charAt(i)>b.charAt(i);
 			}
-		} else if (a.length() < b.length()) {
-
-			isGreater = false;
+			++i;
 		}
 		return isGreater;
 	}
 
 	@Override
 	public int compare(Province arg0, Province arg1) {
-		if (stringIsLess(arg0.getName(), arg1.getName()))
+		if (!stringIsGreater(arg0.getName(), arg1.getName()))
 			return -1;
 		else if (stringIsGreater(arg0.getName(), arg1.getName()))
 			return 1;
